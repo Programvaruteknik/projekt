82e4555773c81c09cdd1aedef3019a167dff88fe
wareus <rien90@hotmail.com>
2015-04-13 19:42:02 +0200

    correlationData without in param

--- DataSource/src/domain/jersey/DataSourceAPI.java
+++ DataSource/src/domain/jersey/DataSourceAPI.java
@@ -4,8 +4,25 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+import domain.api.serialization.JsonParser;
+import domain.datasources.DataSource;
+import domain.datasources.DataSourceFactory;
+import domain.matching.DataMatcher;
+import domain.matching.Resolution;
+import domain.matching.ResultingData;
+
 @Path("/dataSource")
 public class DataSourceAPI
 {
-
+	@GET
+	@Path("/correlationData")
+	public Response getCorrelationData()
+	{
+		DataSource dataSource1 = new DataSourceFactory().getDataSource("TotalFotballGoals");
+		DataSource dataSource2 = new DataSourceFactory().getDataSource("SunAltitudeAtNoon");
+		
+		ResultingData resultingData= new DataMatcher(dataSource1, dataSource2, Resolution.DAY).match();
+		
+		return Response.status(200).entity(new JsonParser().serialize(resultingData)).build();
+	}
 }

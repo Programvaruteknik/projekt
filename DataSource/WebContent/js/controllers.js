e77ac8c3a797eca68340235c0b45b49b16b2e2fb
Jaliee <Jaliee@hotmail.com>
2015-04-17 14:57:08 +0200

    Data source chart now working with real data

--- DataSource/WebContent/js/controllers.js
+++ DataSource/WebContent/js/controllers.js
@@ -1,47 +1,27 @@
 angular.module('controllers', ['googlechart','mm.foundation' ])
-.controller("dataSourceChartController", function($scope){
+.controller("dataSourceChartController", function($scope, $resource){
 	
 	$scope.dataSources = ['TotalFotballGoals', 'SunAltitudeAtNoon'];
 	
 	var selectedDataSources = [];
+	var chartData = [];
 	
 	$scope.select = function (){
 		
 		selected = _.difference($scope.selectedDataSource, selectedDataSources)[0];
 		selectedDataSources = $scope.selectedDataSource;
 		
-		if(selected === 'TotalFotballGoals')
-		{
-			console.log("goals");
-			$scope.chart.data = [
-			                     ['Date', 'Mål'],
-			                     ['2015-01-01',  10],
-			                     ['2015-01-03',  0],
-			                     ['2015-01-05',  12],
-			                     ['2015-01-07',  5],
-			                     ['2015-01-08',  3],
-			                     ];
-			
-			
-		}
-		else if(selected === 'SunAltitudeAtNoon')
-		{
-			console.log("altitude");
-			$scope.chart.data = [
-				                     ['Date', 'altitude'],
-				                     ['2015-01-01',  47],
-				                     ['2015-01-04',  42],
-				                     ['2015-01-06',  44],
-				                     ['2015-01-08',  43],
-				                     ];
-			
-		}
+		$resource("api/dataSource/:dataSource").get({dataSource:selected}, function(data) {
+			console.log(data);
+			chartData = [["test","test"]];
+			angular.forEach(data, function(apiData, key) {
+				chartData.push([key, apiData])
+			});
+			console.log(chartData);
+			$scope.chart.data = chartData;
+		});
 	};
 	
-	
-//	$scope.selected = undefined;
-//	$scope.datasources = ['total football goals', 'the suns altitude at noon'];
-//	
 	$scope.chart = {
 			  "type": "LineChart",
 			  "cssStyle": "height:30em; width:100%;",
@@ -54,42 +34,9 @@ angular.module('controllers', ['googlechart','mm.foundation' ])
 			    
 			  }
 			}
-//	
-//	$scope.onSelect = function(){
-//		
-//		if($scope.selected === 'total football goals')
-//		{
-//			console.log("goals");
-//			$scope.chart.data = [
-//			                     ['Date', 'Mål'],
-//			                     ['2015-01-01',  10],
-//			                     ['2015-01-03',  0],
-//			                     ['2015-01-05',  12],
-//			                     ['2015-01-07',  5],
-//			                     ['2015-01-08',  3],
-//			                     ];
-//			
-//			
-//		}
-//		else if($scope.selected === 'the suns altitude at noon')
-//		{
-//			console.log("altitude");
-//			$scope.chart.data = [
-//				                     ['Date', 'altitude'],
-//				                     ['2015-01-01',  47],
-//				                     ['2015-01-04',  42],
-//				                     ['2015-01-06',  44],
-//				                     ['2015-01-08',  43],
-//				                     ];
-//			
-//		}
-//	};
-
-	
 })
-.controller("correlationChartController", function($scope, $resource){
-	
 
+.controller("correlationChartController", function($scope, $resource){
 	
 	$scope.dataSources = ['TotalFotballGoals', 'SunAltitudeAtNoon'];
 	
@@ -100,8 +47,8 @@ angular.module('controllers', ['googlechart','mm.foundation' ])
 			$resource("api/dataSource/correlationData/:dataSource1/:dataSource2").get({dataSource1:$scope.selectedDataSource[0],dataSource2:$scope.selectedDataSource[1]}, function(data) {
 				
 				var chartData = [["test","test"]];
-				angular.forEach(data.resultData, function(ApiData, key) {
-					chartData.push([ApiData.x,ApiData.y])
+				angular.forEach(data.resultData, function(apiData, key) {
+					chartData.push([apiData.x,apiData.y])
 				});
 				
 				$scope.chart.data = chartData;
@@ -110,7 +57,6 @@ angular.module('controllers', ['googlechart','mm.foundation' ])
 		}
 		
 	};
-	
 	
 	$scope.chart = {
 			"type": "ScatterChart",
@@ -121,10 +67,6 @@ angular.module('controllers', ['googlechart','mm.foundation' ])
 				title: 'Correlation Chart',
 				pointSize: 12,
 				legend: 'none'
-				
 			}
 	}
-
-	
-	
 });

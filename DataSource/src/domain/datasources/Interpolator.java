098ae8b11998bcb3d0b37a365f95ce1a8cccc4ee
RadarJam <ostberg.rasmus@gmail.com>
2015-04-19 23:12:10 +0200

    Fixed interpolator.

--- DataSource/src/domain/datasources/Interpolator.java
+++ DataSource/src/domain/datasources/Interpolator.java
@@ -3,6 +3,8 @@ package domain.datasources;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
 /**
  * Creates the missing dates which are not found in a Map. The new entries will
@@ -15,13 +17,14 @@ public class Interpolator {
 	/**
 	 * Returns the date which occurs first in the map.
 	 * 
-	 * @param sorted
+	 * @param sc
 	 *            The Map.
 	 * @return LocalDate The first date.
 	 */
-	protected LocalDate getFirst(Map<LocalDate, Double> sorted) {
+	protected LocalDate getFirst(DataSource sc) {
+		Map<LocalDate, Double> map = sc.getData();
 		int counter = 0;
-		for (LocalDate date : sorted.keySet()) {
+		for (LocalDate date : map.keySet()) {
 			if (counter == 0) {
 				return date;
 			}
@@ -32,14 +35,15 @@ public class Interpolator {
 	/**
 	 * Returns the date which occurs last.
 	 * 
-	 * @param sorted
+	 * @param sc
 	 *            The map.
 	 * @return LocalDate The Date.
 	 */
-	protected LocalDate getLast(Map<LocalDate, Double> sorted) {
-		int lastIndex = sorted.size() - 1;
+	protected LocalDate getLast(DataSource sc) {
+		Map<LocalDate, Double> map = sc.getData();
+		int lastIndex = map.size() - 1;
 		int counter = 0;
-		for (LocalDate date : sorted.keySet()) {
+		for (LocalDate date : map.keySet()) {
 			if (counter == (lastIndex)) {
 				return date;
 			}
@@ -51,13 +55,13 @@ public class Interpolator {
 	/**
 	 * Returns the period where all the dates in the map are occurring.
 	 * 
-	 * @param map
+	 * @param sc
 	 *            The map.
 	 * @return int The length (measuered in days) of the period.
 	 */
-	protected int getPeriod(Map<LocalDate, Double> map) {
-		LocalDate first = getFirst(map);
-		LocalDate last = getLast(map);
+	protected int getPeriod(DataSource sc) {
+		LocalDate first = getFirst(sc);
+		LocalDate last = getLast(sc);
 		Period p = first.until(last);
 
 		int distanceInDays = p.getDays();
@@ -67,18 +71,43 @@ public class Interpolator {
 
 	/**
 	 * The map are filled with dates which are considered as missing.
-	 * @param map 
+	 * 
+	 * @param sc
 	 *            The map which will be filled with the missing dates.
 	 */
-	public void fillOutMissingDays(Map<LocalDate, Double> map) {
-		int distance = getPeriod(map);
-		LocalDate first = getFirst(map);
+	public DataSource fillOutMissingDays(DataSource sc) {
+		TreeMap<LocalDate, Double> map = sc.getData();
+		TreeMap<LocalDate, Double> copyMap = new TreeMap<>();
+		LocalDate first = getFirst(sc);
+		LocalDate current = first;
+		int days = getPeriod(sc);
 
-		for (int i = 0; i < distance; i++) {
-			LocalDate tmp = first.plusDays(i);
-			if (!map.containsKey(tmp)) {
-				map.put(tmp, null);
+		for (int i = 0; i < days + 1; i++) {
+			if (map.containsKey(current)) {
+				copyMap.put(current, map.get(current));
+			} else {
+				copyMap.put(current, null);
 			}
+			current = current.plusDays(1);
 		}
+
+		DataSource copySource = new DataSource() {
+
+			@Override
+			public String getUnit() {
+				return sc.getUnit();
+			}
+
+			@Override
+			public String getName() {
+				return sc.getName();
+			}
+
+			@Override
+			public TreeMap<LocalDate, Double> getData() {
+				return copyMap;
+			}
+		};
+		return copySource;
 	}
 }
--- DataSource/test/domain/datasources/TestInterpolatedDates.java
+++ DataSource/test/domain/datasources/TestInterpolatedDates.java
@@ -1,30 +1,28 @@
 package domain.datasources;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
-import java.util.Collections;
 import java.util.Map;
 import java.util.TreeMap;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import domain.api.serialization.JsonParser;
-
 public class TestInterpolatedDates {
 	// Hur många datum som ska sättas in
 	// Färdig interpolerad lista.
 
-	Map<LocalDate, Double> map = new TreeMap<>();
+	TreeMap<LocalDate, Double> map = new TreeMap<>();
 	Interpolator interpolator;
 	String expectedDate;
-
+	DataSource sc = mock(DataSource.class);
+	
 	@Before
 	public void setup() {
+		when(sc.getData()).thenReturn(map);
 		map.put(LocalDate.parse("2001-01-02"), 2.0);
 		map.put(LocalDate.parse("2001-01-01"), 1.0);
 
@@ -34,20 +32,21 @@ public class TestInterpolatedDates {
 	@Test
 	public void tesGetFirst() {
 		expectedDate = "2001-01-01";
-		assertEquals(LocalDate.parse(expectedDate), interpolator.getFirst(map));
+		assertEquals(LocalDate.parse(expectedDate), interpolator.getFirst(sc));
 	}
 
 	@Test
 	public void testGetLast() {
 		expectedDate = "2001-01-02";
-		assertEquals(LocalDate.parse(expectedDate), interpolator.getLast(map));
+		assertEquals(LocalDate.parse(expectedDate), interpolator.getLast(sc));
 	}
 
 	@Test
 	public void testGetDistanceInDays() {
 		int expectedDistance = 6;
 		map.put(LocalDate.parse("2001-01-07"), 7.0);
-		int distance = interpolator.getPeriod(map);
+		
+		int distance = interpolator.getPeriod(sc);
 		assertEquals(expectedDistance, distance);
 	}
 
@@ -56,9 +55,12 @@ public class TestInterpolatedDates {
 		LocalDate nullDate = LocalDate.parse("2001-01-03");
 		LocalDate endDate = LocalDate.parse("2001-01-04");
 		map.put(endDate, 4.0);
-		interpolator.fillOutMissingDays(map);
-		assertEquals(null, map.get(nullDate));
-		assertEquals(4, map.size());
+		
+		DataSource newSrc = interpolator.fillOutMissingDays(sc);
+		
+		Map<LocalDate,Double> mapInSource = newSrc.getData();
+		assertEquals(null, mapInSource.get(nullDate));
+		assertEquals(4, mapInSource.size());
 	}
 	
 }

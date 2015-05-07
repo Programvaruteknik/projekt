3800d0846efd40b2f042a431ed909a12469120ee
wareus <rien90@hotmail.com>
2015-05-07 15:12:56 +0200

    mod button implemented

--- DataSource/WebContent/js/directives.js
+++ DataSource/WebContent/js/directives.js
@@ -3,6 +3,19 @@ angular.module('directives', [])
 	
 	return {
 		restrict: 'E',
-		templateUrl: './html/directives/modTabel.html'
+		templateUrl: './html/directives/modTabel.html',
+		controller: function($scope){
+			
+			$scope.addMod = function(){
+				
+				$scope.modList.push({
+					dataSourceName:"Solens altitud vid 12",
+					year:0,
+					month:0,
+					days:-1
+						});
+				$scope.updateChart();
+			}
+		}
 	}
 });
